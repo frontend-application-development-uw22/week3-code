7c6cf07ea8acee18c075b1bd144767cdca90f605
Brian Hague <bhague1281@gmail.com>
2022-04-28 20:37:11 -0700

    countdown example

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function Counter() {
@@ -20,10 +20,42 @@ function Counter() {
   )
 }
 
+function Countdown() {
+  const [count, setCount] = useState(10);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (count === 0) {
+        return clearInterval(interval);
+      }
+
+      console.log('count', count);
+      setCount(count - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+
+  return (
+    <div>
+      <h1>Countdown</h1>
+      <h2>{count}</h2>
+    </div>
+  )
+}
+
 function App() {
+  const [showCountdown, toggleCountdown] = useState(false);
+
   return (
     <div className="App">
       <Counter />
+      <hr />
+      <button onClick={() => toggleCountdown(!showCountdown)}>
+        Toggle Countdown
+      </button>
+      {showCountdown ? <Countdown /> : null}
     </div>
   );
 }
